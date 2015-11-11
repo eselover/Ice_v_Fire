5d2b4c509645dc3e132d3779fbc2f509e3e9a4cb
Eric Selover <eric.selover@gmail.com>
2015-11-11 00:35:08 -0500

    Added a main menu
    Start of a main menu, for playing, options, and quit

---
+++ src/Ice_v_Fire/Assets/Scripts/MainMenuControllers.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuControllers : MonoBehaviour {
+
+	public void Play() {
+		Application.LoadLevel ("1");
+	}
+
+	public void Exit() {
+		Application.Quit ();
+	}
+
+	public void Options() {
+		//TODO: Code another UI Menu for the options screen
+	}
+}
